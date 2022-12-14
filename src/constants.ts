9fc8c67188ddb9f0921531de26291362cf990bda
Sidney Nemzer <SidneyNemzer@users.noreply.github.com>
2022-12-13 20:51:21 -0500

    fix container selector
    Selector stopped working because GitHub added a div between `body` and
    `.application-main`. I don't know why I made this so specific anyway.
    
    Fixes #10

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const NOT_FOUND_PAGE_SELECTOR =
   'img[alt="404 “This is not the web page you are looking for”"]';
 
 export const CONTAINER_PARENT_SELECTOR =
-  "body > div.application-main.d-flex.flex-auto.flex-column > main > div.position-relative";
+  ".application-main > main > .position-relative";
